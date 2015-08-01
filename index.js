d6975dd740eb467bda3ae2c7d40e4fd7ce1d0041
Myztiq <github@ryan-kahn.com>
2015-07-31 18:51:59 -0700

    Updated to us a more reasonable cookie size

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 var tough = require('tough-cookie');
+var crypto = require('crypto');
 
 
 var cookieJar = new tough.CookieJar();
 var memwatch = require('memwatch');
 var hd = new memwatch.HeapDiff();
-var times = 10000000;
+var times = 1000000;
+
+
 
 var start = new Date();
 
-console.log('Running ' + times);
-for(var counter=0; counter<=times; counter++){
-  if(counter % (times/1000) === 0) {
-    console.log('Testing: ' + (counter/times * 100).toFixed(1) + '% (' + counter + ')');
-  }
-  if (counter === times) {
-    console.log('Finished running ' + counter + ' times. Phew. That took ' + ((new Date()-start)/1000/60).toFixed(2) + ' Minutes');
-    var diff = hd.end();
-    console.log(diff);
-    console.log('Top offenders in order:');
-    for(var i=0;i<4;i++){
-      console.log(diff.change.details[i]);
+crypto.randomBytes(1000, function (e, cookieContents) {
+  var cookie = 'sid='+cookieContents.toString('hex')+'; Domain=example.com';
+  console.log('Running ' + times + ' with a cookie of length: ', cookie.length);
+  for(var counter=0; counter<=times; counter++){
+    if(counter % (times/1000) === 0) {
+      console.log('Testing: ' + (counter/times * 100).toFixed(1) + '% (' + counter + ')');
     }
-    return;
+    if (counter === times) {
+      console.log('Finished running ' + counter + ' times. Phew. That took ' + ((new Date()-start)/1000/60).toFixed(2) + ' Minutes');
+      var diff = hd.end();
+      console.log(diff);
+      console.log('Top offenders in order:');
+      for(var i=0;i<4;i++){
+        console.log(diff.change.details[i]);
+      }
+      return;
+    }
+
+    cookieJar.setCookie(cookie, 'http://example.com/index.html', {}, function () {});
   }
-  cookieJar.setCookie('a=b; Domain=example.com', 'http://example.com/index.html', {}, function () {});
-}
+});
